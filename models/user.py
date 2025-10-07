0856b5f5e1ec77e60ac563533fca83ea8df9fe97
k-bridget <k.bridget@alustudent.com>
2025-10-07 06:15:36 -0700

    modified user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" User module for AirBnB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Representation of a User in the database"""
+
     __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    places = relationship('Place', backref='user', cascade='delete')
-    reviews = relationship('Review', backref='user', cascade='delete')
