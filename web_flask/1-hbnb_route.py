450f6307194b2792a80376b233c84cfd1f7a2804
k-bridget <k.bridget@alustudent.com>
2025-10-27 08:57:16 +0000

    modified files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+start Flask application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Return a greeting at root route"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return a short message for /hbnb"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
